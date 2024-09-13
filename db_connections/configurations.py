22993837e2de6b1763a092ea4e519cc924760c37
komal050505 <komal050505@gmail.com>
2024-09-14 02:06:47 +0530

    changes made

--- db_connections/configurations.py
+++ db_connections/configurations.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 
 # Database configuration
 DATABASE_URL = "postgresql://postgres:1995@localhost:5432/postgres"
-YOUR_GOOGLE_MAPS_API_KEY = "AIzaSyCCu1t51MWdiAr0aff34xQTwWjiUAhb3yA"
+YOUR_GOOGLE_MAPS_API_KEY = ""
 
 # SQLAlchemy setup
 Base = declarative_base()
